686c748c30685fd211ef1ae354f115be3cdd29aa
Breno Romeiro <bsromeiro@gmail.com>
2023-02-03 17:32:11 -0300

    finish exercise 1 + 1/2 exercise 2

--- 1.map-filter-find.js
+++ 1.map-filter-find.js
@@ -1,11 +1,32 @@
 const getPokeNames = (pokemons) => {
-    return pokemons.map(pokemon => pokemon.name)
-}
+  return pokemons.map((pokemon) => pokemon.name);
+};
 
 const getPokemonById = (pokemons, id) => {
-}
+  return pokemons[id - 1];
+};
+
+const getRarePokemons = (pokemons) => {
+  return pokemons.filter((pokemon) => pokemon.spawn_chance < 0.1);
+};
+
+const getMidSizedPokemon = (pokemons) => {
+  return pokemons.find((pokemon) => pokemon.weight === "38.0 kg");
+};
+
+const getAdultPokemons = (pokemons) => {
+  return pokemons.filter((pokemon) => pokemon.egg === "Not in Eggs");
+};
+
+const getPokemonImages = (pokemons) => {
+  return pokemons.map((pokemon) => pokemon.img);
+};
 
 module.exports = {
-    getPokeNames,
-    getPokemonById,
-}
+  getPokeNames,
+  getPokemonById,
+  getRarePokemons,
+  getMidSizedPokemon,
+  getAdultPokemons,
+  getPokemonImages,
+};
--- 2.reduce.js
+++ 2.reduce.js
@@ -1,13 +1,52 @@
 const calculateTotalPokemonWeight = (pokemons) => {
-    return pokemons.reduce((totalWeight, currentPokemon) => {
-        return totalWeight + parseFloat(currentPokemon.weight)
-    }, 0)
-}
+  return pokemons.reduce((totalWeight, currentPokemon) => {
+    return totalWeight + parseFloat(currentPokemon.weight);
+  }, 0);
+};
 
 const calculateAverageSpawnChance = (pokemons) => {
-}
+  const summedSpawnChance = pokemons.reduce(
+    (totalSpawnChance, currentPokemon) => {
+      return totalSpawnChance + currentPokemon.spawn_chance;
+    },
+    0
+  );
+  return summedSpawnChance / pokemons.length;
+};
+
+const calculateTotalEggDistance = (pokemons) => {
+  return pokemons
+    .filter((pokemon) => {
+      return pokemon.egg !== "Not in Eggs";
+    })
+    .map((pokemon) => parseFloat(pokemon.egg))
+    .reduce((totalDistance, currentNum) => {
+      return totalDistance + currentNum;
+    }, 0);
+};
+
+// const calculateTotalEggDistance = (pokemons) => {};
+
+// {
+//     id: 99,
+//     num: '099',
+//     name: 'Kingler',
+//     img: 'http://www.serebii.net/pokemongo/pokemon/099.png',
+//     type: [ 'Water' ],
+//     height: '1.30 m',
+//     weight: '60.0 kg',
+//     candy: 'Krabby Candy',
+//     egg: 'Not in Eggs',
+//     spawn_chance: 0.062,
+//     avg_spawns: 6.2,
+//     spawn_time: '03:44',
+//     multipliers: null,
+//     weaknesses: [ 'Electric', 'Grass' ],
+//     prev_evolution: [ [Object] ]
+//   },
 
 module.exports = {
-    calculateTotalPokemonWeight,
-    calculateAverageSpawnChance,
-}
+  calculateTotalPokemonWeight,
+  calculateAverageSpawnChance,
+  calculateTotalEggDistance,
+};
